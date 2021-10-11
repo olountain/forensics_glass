4e5ca26bcee6cda01bd6d65ab0969652c41a23bd
Oliver Lountain <oliverlountain@Olivers-iMac.local>
2021-10-11 12:16:07 +1030

    Added basic manipulation and results for uploaded data

--- App/glass_comparison/app.R
+++ App/glass_comparison/app.R
@@ -15,6 +15,8 @@ data <- read_csv("../../Data/202106_clean_data_aus.csv") %>%
 ctrl_data <- data %>% filter(obj == "760-27")
 rec_data <- data %>% filter(obj == "760-16")
 
+# bind_rows(ctrl_data, rec_data) %>% write_csv(file = "Data/clean_test_data.csv")
+
 
 # setup interval criteria
 source("../../Code/standard_criterion.R")
@@ -80,7 +82,7 @@ ui <- material_page(
         # Place side-nav tabs within side-nav
         material_side_nav_tabs(
             side_nav_tabs = c(
-                "Data Upload" = "data_upload",
+                "Data" = "data",
                 "Interval Criteria" = "interval_ritera",
                 "Decision Tree" = "decision_tree"
             ),
@@ -89,23 +91,45 @@ ui <- material_page(
     ),
     # Define side-nav tab content
     material_side_nav_tab_content(
-        side_nav_tab_id = "data_upload",
+        side_nav_tab_id = "data",
         
         material_card(
-            tags$h3("Data Upload")
+            tags$h3("Data")
+        ),
+        
+        material_file_input(
+            input_id = "file_upload",
+            label = "Upload Data"
+        ),
+        
+        material_card(
+            tags$h5("Uploaded Data"),
+            
+            # textOutput("upl_success"),
+            
+            div(style = 'overflow-x: scroll', dataTableOutput('upl_data_tbl')),
+            
+            div(style = 'overflow-x: scroll', dataTableOutput('upl_ctrl_tbl')),
+            
+            div(style = 'overflow-x: scroll', dataTableOutput('upl_rec_tbl'))
         ),
         
         material_card(
             tags$h5("Control Data"),
             
-            div(style = 'overflow-x: scroll', tableOutput('ctrl_data'))
+            div(style = 'overflow-x: scroll', dataTableOutput('ctrl_data_tbl'))
         ),
         
         material_card(
             tags$h5("Recovered Data"),
             
-            div(style = 'overflow-x: scroll', tableOutput('rec_data'))
+            div(style = 'overflow-x: scroll', dataTableOutput('rec_data_tbl'))
         ),
+        
+        material_button(
+            input_id = "calculate",
+            label = "Calculate"
+        )
         
     ),
     
@@ -192,35 +216,220 @@ ui <- material_page(
 
 server <- function(input, output) {
     
-    ## example data to display
+## old example data to display -------------------------------------------------------------------------------
     
-    output$ctrl_data <- renderTable(ctrl_data %>%
-                                        clean_names(sep_in = "_ppm_m", case = "big_camel") %>% 
-                                        select(-c(Type,Rep,DateTime,DurationS,Li7)))
+    # output$ctrl_data_tbl <- renderDataTable(ctrl_data %>%
+    #                                         clean_names(sep_in = "_ppm_m", case = "big_camel") %>% 
+    #                                         select(-c(Type,Rep,DateTime,DurationS,Li7)) %>% 
+    #                                             datatable())
+    # 
+    # output$rec_data_tbl <- renderDataTable(rec_data %>%
+    #                                            clean_names(sep_in = "_ppm_m", case = "big_camel") %>% 
+    #                                            select(-c(Type,DateTime,DurationS,Li7)) %>% 
+    #                                            datatable())
     
-    output$rec_data <- renderTable(rec_data %>%
-                                        clean_names(sep_in = "_ppm_m", case = "big_camel") %>% 
-                                        select(-c(Type,DateTime,DurationS,Li7)))
+## uploaded data -----------------------------------------------------------------------------------------
+
+    uploaded_data  <- reactive({
+        inFile <- input$file_upload
+
+        if (is.null(inFile)){
+            return(NULL)
+        }
+        # showTab(inputId = "tabs", target = "Uploaded data")
+        read_csv(inFile$datapath)
+    })
     
-    ## interval criteria output
+    uploaded_ctrl <- reactive({
+        if (is.null(uploaded_data())){
+            return(NULL)
+        }
+        uploaded_data() %>% filter(type == "control")
+    })
+    
+    uploaded_rec <- reactive({
+        if (is.null(uploaded_data())){
+            return(NULL)
+        }
+        uploaded_data() %>% filter(type == "recovered")
+    })
+    
+    
+    output$upl_data_tbl <- renderDataTable({
+        uploaded_data()
+    })
+    
+    output$upl_ctrl_tbl <- renderDataTable({
+        uploaded_ctrl()
+    })
+    
+    output$upl_rec_tbl <- renderDataTable({
+        uploaded_rec()
+    })
+    
+    
+    
+    
+    
+## old upload stuff ------------------------------------------------------------------------------------
+    # output$upl_success <- renderText({
+    #     req(input$file_upload)
+    #     
+    #     tryCatch(
+    #         {
+    #             out <- "Upload Successful"
+    #         },
+    #         error = function(e){
+    #             stop(safeError(e))
+    #         }
+    #     )
+    #     
+    #     return(out)
+    # })
+    # 
+    # output$upl_data_tbl <- renderDataTable({
+    #     req(input$file_upload)
+    # 
+    #     tryCatch(
+    #         {
+    #             df <- read_csv(input$file_upload$datapath)
+    #         },
+    #         error = function(e){
+    #             stop(safeError(e))
+    #         }
+    #     )
+    # 
+    #     return(df %>% datatable())
+    # })
+
+## ----
+    ## perform calculations
+    
+    # eventReactive(
+    #     "calculate",
+    #     {
+    #         
+    #     }
+    #               )
+    
+## interval criteria output -----------------------------------------------------------------------
     # standard
-    # output$res_std <- renderText(std_res$match[1])
-    # output$dist_std <- renderText(round(std_res$distance[1], 3))
-    output$table_all <- renderTable(all_res, digits = 3)
     
-    output$table_differences <- renderTable(std_res %>%
-                                        unnest(distances) %>% 
-                                        select(-c(Match:Score)) %>% 
-                                        clean_names(sep_in = "_ppm_m", case = "big_camel"),
-                                        digits = 3)
+    interval_results <- reactive({
+        
+        if (is.null(uploaded_data())){
+            return(NULL)
+        }
+        
+        std_res <- standard_criterion(uploaded_ctrl(), uploaded_rec())
+        ellips_res <- ellipsoid_criterion(uploaded_ctrl(), uploaded_rec())
+        
+        all_res <- bind_cols(std_res[,1:3], ellips_res[,2:3]) %>% 
+            rename(`Standard Match` = Match...2,
+                   `Standard Score` = Score...3,
+                   `Ellipsoid Match` = Match...4,
+                   `Ellipsoid Score` = Score...5) %>% 
+            mutate(Agree = ifelse(`Standard Match` == `Ellipsoid Match`,
+                                  "Yes", "No"))
+        
+        return(all_res)
+
+        
+    })
     
-    # ellipsois
+    output$table_all <- renderTable(
+        {
+            if (is.null(interval_results())){
+                return(NULL)
+            }
+            return(interval_results())
+        },
+        digits = 3
+    )
+    
+    output$table_differences <- renderTable(
+        {
+            if (is.null(uploaded_data())){
+                return(NULL)
+            }
+            standard_criterion(uploaded_ctrl(), uploaded_rec()) %>%
+                unnest(distances) %>% 
+                select(-c(Match:Score)) %>% 
+                clean_names(sep_in = "_ppm_m", case = "big_camel")
+        },
+        digits = 3
+        )
+    
+    
+    
+## old interval results ---------------------------------------------------------------------------
+    
+    
+    
+    # tmp <- input$ctrl_data_tbl_rows_selected
+    
+    # output$table_all <- renderTable(all_res, digits = 3)
+    
+    # output$table_differences <- renderTable(std_res %>%
+    #                                     unnest(distances) %>% 
+    #                                     select(-c(Match:Score)) %>% 
+    #                                     clean_names(sep_in = "_ppm_m", case = "big_camel"),
+    #                                     digits = 3)
+    
+    # ellipsoid
     # output$res_ellips <- renderText(ellips_res$Match[1])
-    output$table_ellips <- renderTable(ellips_res)
+    # output$table_ellips <- renderTable(ellips_res)
     
-    ## decision tree output
+
+## decision tree output ------------------------------------------------------------------------
+    
+    decision_tree_results <- reactive({
+        
+        if (is.null(uploaded_data())){
+            return(NULL)
+        }
+        
+        ctrl_mean <- uploaded_ctrl() %>%
+            select(mg_ppm_m24:last_col()) %>%
+            summarise_all(mean)
+        
+        rec_means <- uploaded_rec() %>%
+            group_by(frag) %>%
+            select(mg_ppm_m24:last_col()) %>%
+            summarise_all(mean)
+        
+        rec_diffs <- rec_means
+        
+        for (i in 1:nrow(rec_means)) {
+            rec_diffs[i, 2:18] <- abs(ctrl_mean - rec_means[i, 2:18])
+        }
+        
+        tree_res <- vector(mode = "list", length = nrow(rec_diffs))
+        
+        for (i in 1:nrow(rec_diffs)) {
+            tree_res[[i]] <- test_tree(rec_diffs[i,])
+            tree_res[[i]]$probs <- max(tree_res[[i]]$probs)
+        }
+        
+        tree_res <- tree_res %>%
+            map_dfr(as_tibble) %>%
+            rename(Match = class, Probability = probs)
+        
+        
+        return(tree_res)
+        
+        
+    })
+    
+    
+    
+    
+    
+    
+    
+    
     # results
-    output$dec_tree_res <-renderTable(tree_res)
+    output$dec_tree_res <-renderTable(decision_tree_results())
     # output$dec_tree_prob <-renderText(round(tree_res$prob[1],3))
     
     # decision tree logic
